abb23e964f9239165b412954ce9b14f753b7ef8d
Subramanya Shenoy <subu@Subramanyas-Mac-mini.local>
2025-07-21 18:38:16 +0530

    relogin loop fixed

--- frontend/src/services/supabase.ts
+++ frontend/src/services/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import getSupabaseConfig from '../config/supabase.config';
 import { Database } from '../types/database';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const config = getSupabaseConfig();
 
@@ -9,6 +10,11 @@ export const supabase = createClient<Database>(config.url, config.anonKey, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true,
+    storage: {
+      getItem: (key: string) => AsyncStorage.getItem(key),
+      setItem: (key: string, value: string) => AsyncStorage.setItem(key, value),
+      removeItem: (key: string) => AsyncStorage.removeItem(key),
+    },
   },
 });
 
